506be72c7b893f009f505f97a638b8bc809d5203
inkaink <inka1211@mail.ru>
2022-04-24 12:01:21 +0300

    modul-5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
+import { dataSouvenir } from './dataSouvenir';
 import './App.css';
 
 function App() {
+
+  const [landmark, setLandmark] = useState(0);
+  const {id, name, description, image, showMore} = data[landmark];
+
+  const [showText, setShowText] = useState(false);
+  
+  const [souvenirs, setSouvenirs] = useState(dataSouvenir);
+
+  const previousLandmark = () => {
+    setLandmark((landmark => {
+      landmark --;
+      if(landmark < 0) {
+        return data.length-1;
+      }
+      return landmark;
+    }))
+  }
+
+  const nextLandmark = () => {
+    setLandmark((landmark => {
+      landmark ++;
+      if(landmark > data.length-1) {
+        landmark = 0;
+      }
+      return landmark;
+    }))
+  }
+
+  const showTextClick = (landmark) => {
+    landmark.showMore = !landmark.showMore;
+    setShowText(!showText);
+  }
+
+  const removeSouvenir = (idSouvenir) => {
+    let newMassivSouvenir = souvenirs.filter(souvenir => souvenir.idSouvenir !== idSouvenir);
+    setSouvenirs(newMassivSouvenir);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <div className='container'>
+          <h2>Достопримечательности Лондона</h2>
+        </div>
+
+        <div className='container'>
+          <h2>{id} - {name}</h2>
+        </div>
+
+        <div className='container'>
+          <img src={image} width="300px" alt="foto"/>
+        </div>
+
+        <div className='container'>
+          <h3>{showMore ? description : description.substring(0, 200) + "..."}
+          <button onClick={() => showTextClick(landmark)}>{showMore ? "Показать меньше" : "Показать больше"}</button>
+          </h3>
+        </div>
+
+        <div className='container'>
+          <button onClick={previousLandmark}>Previous</button>
+          <button onClick={nextLandmark}>Next</button>
+        </div>
+
+
+        
+          {souvenirs.map((souvenir => {
+            const {idSouvenir, nameSouvenir, imageSouvenir} = souvenir;
+
+            return(
+              <div key={id}>
+                <div className='container'>
+                  <h3>{idSouvenir} - {nameSouvenir}</h3>
+                </div>
+
+                <div className='container'>
+                  <img src={imageSouvenir} width="200px" alt="souvenir" />
+                </div>
+
+                <div className='container'>
+                  <button onClick={() => removeSouvenir(idSouvenir)}>Удалить</button>
+                </div>
+              </div>
+            )
+          }))}
+
+          <div className='container'>
+            <button onClick={() => setSouvenirs([])}>Удалить все</button>
+          </div>
+        
+
+
     </div>
   );
 }
---
+++ src/dataSouvenir.js
@@ -0,0 +1,32 @@
+export const dataSouvenir = [
+    {
+        idSouvenir: 1,
+        nameSouvenir: 'Сувенир с изображением английского флага',
+        imageSouvenir: "https://images.unsplash.com/photo-1501406606344-f28a12920fc4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+        idSouvenir: 2,
+        nameSouvenir: 'Знаменитый английский чай',
+        imageSouvenir: "https://www.bygeo.ru/uploads/posts/2018-04/thumbs/1523188242_13-chay.jpg"
+    },
+    {
+        idSouvenir: 3,
+        nameSouvenir: 'Чайный сервиз',
+        imageSouvenir: "https://images.unsplash.com/photo-1516831083365-0efd347838f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    },
+    {
+        idSouvenir: 4,
+        nameSouvenir: 'Мишка Паддингтон',
+        imageSouvenir: "https://www.bygeo.ru/uploads/posts/2018-04/thumbs/1523188281_03.jpg"
+    },
+    {
+        idSouvenir: 5,
+        nameSouvenir: 'Сувениры из «Гарри Поттера»',
+        imageSouvenir: "https://london10.ru/sites/london10.ru/files/styles/large_640_425_/public/3403910847_6cb8be203d_c.jpg"
+    },
+    {
+        idSouvenir: 6,
+        nameSouvenir: 'Сувенир английской телефонной будки',
+        imageSouvenir: "https://london10.ru/sites/london10.ru/files/styles/large_640_425_/public/field/image/booth-16817_640.jpg?itok=OtXXe2Pb"
+    }
+]
